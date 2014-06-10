fe16bf554a686f540240698ae43b17abed102a35
felixZhuVip <felix.zhu@vipshop.com>
2014-06-10 11:47:46 +0800

    Update VSHorizontalLayoutView.m
    finish the HorizontalLayout.

--- LayoutContainer/VSHorizontalLayoutView.m
+++ LayoutContainer/VSHorizontalLayoutView.m
@@ -4,9 +4,47 @@
 //
 
 #import "VSHorizontalLayoutView.h"
+#import "VSHorizontalLayout.h"
 
+@interface VSHorizontalLayoutView()<VSHorizontalLayoutDelegate>
+
+@end
 
 @implementation VSHorizontalLayoutView {
-
+    VSHorizontalLayout *_HorizontalLayout;
 }
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        _HorizontalLayout = [[VSHorizontalLayout alloc] init];
+        _HorizontalLayout.delegate = self;
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [_HorizontalLayout release];
+    [super dealloc];
+}
+
+- (void)addSubviewForLayout:(UIView *)subview {
+    [_HorizontalLayout addSubviewForLayout:subview];
+    [super addSubview:subview];
+}
+
+- (void)willRemoveSubview:(UIView *)subview {
+    [_HorizontalLayout removeSubviewForLayout:subview];
+    [super willRemoveSubview:subview];
+}
+
+-(void)updateLayout {
+    [_HorizontalLayout updateLayout];
+}
+
+#pragma mark - VSHorizontalLayoutDelegate
+
+-(void)layoutViewDidChange:(VSHorizontalLayout*)HorizontalLayout {
+}
+
 @end
